fe97c9bdcccbe309d45f9758102554f025ea6754
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-10-01 23:59:13 +0200

    Add files via upload

---
+++ Radio button kontrola/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio_button_kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRezerviraj_Click(object sender, EventArgs e)
+        {
+            TxtOdabir.Clear();
+
+            if (RBtnAutobus.Checked)
+            {
+                TxtOdabir.AppendText("Odabrali ste prijevoz autobusom.");
+            }
+            if (RBtnAvion.Checked)
+            {
+                TxtOdabir.AppendText("Odabrali ste prijevoz avionom.");
+            }
+            if (RBtnVlastiti.Checked)
+            {
+                TxtOdabir.AppendText("Odabrali ste prijevoz vlastitim vozilom.");
+            }
+        }
+    }
+}
